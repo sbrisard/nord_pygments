b69f3cf9763c180eee7063e4aae5a2c9f55b3086
Sébastien Brisard <sebastien.brisard@m4x.org>
2019-12-26 20:33:46 +0100

    v0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nord_pygments',
-      version=0.0,
+      version=0.1,
       py_modules=['nord_pygments'],
       license='BSD-3',
       entry_points={'pygments.styles': 'nord = nord_pygments:NordStyle'}
