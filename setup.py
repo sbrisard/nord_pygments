ff245bbb13d68010f093ddb0297c9d2d7c79df41
Sébastien Brisard <sebastien.brisard@m4x.org>
2019-12-26 20:26:04 +0100

    Rename project

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
-setup(name='pygnord',
+setup(name='nord_pygments',
       version=0.0,
-      py_modules=['pygnord'],
+      py_modules=['nord_pygments'],
       license='BSD-3',
-      entry_points={'pygments.styles': 'nord = pygnord:NordStyle'}
+      entry_points={'pygments.styles': 'nord = nord_pygments:NordStyle'}
 )
